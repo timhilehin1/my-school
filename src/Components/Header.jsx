60cbb0c780295e40df7363cd9f1b3a3b7a413366
Timilehin <timilehinoladapo0@gmail.com>
2023-04-17 07:00:39 +0100

    feat : header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header(){
+return (
+    <>
+    <section className="header container mx-auto">
+     <p className="text-center py-2 md:hidden">REGISTER |  LOGIN</p>
+    </section>
+     <hr className="bg-[#374151]"/>
+     </>
+)
+}
+
+export default Header
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar(){
+
+    return (
+        <section className="flex px-2 py-3 header justify-between container mx-auto">
+         <p>logo</p>
+         <p>icon</p>
+        </section>
+    )
+}
+
+export default Navbar
