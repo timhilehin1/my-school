9bea8ee456536591496a4d0075dae14c69011c70
Cindy Eke <offcialcee1@gmail.com>
2023-04-17 17:03:58 +0100

    chnage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,16 @@ function Header(){
 return (
     <>
     <section className="header container mx-auto">
-     <p className="text-center py-2 md:hidden">REGISTER |  LOGIN</p>
+            <p className="text-center py-2 md:hidden">REGISTER |  LOGIN</p>
+            <div className="hidden md:flex justify-between px-8 py-2">
+            <ul className="flex gap-6 items-center">Information for:
+            <li>Students</li>
+            <li>Teachers</li>
+            <li>Research</li>
+            <li>Community</li>
+            </ul>
+            <p>REGISTER |  LOGIN</p>
+            </div>
     </section>
      <hr className="bg-[#374151]"/>
      </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,18 @@
+import React from 'react'
+import logo from '../assets/small logo.png'
+import { AiOutlineMenu} from "react-icons/ai";
+
 
 
 function Navbar(){
 
     return (
-        <section className="flex px-2 py-3 header justify-between container mx-auto">
-         <p>logo</p>
-         <p>icon</p>
+        <section className="flex px-2 py-3 header justify-between  items-center container mx-auto">
+            <div className='flex gap-1.5 items-center'>
+                <img className='h-11' src={logo} alt="School's logo" />
+                <p>Victoria</p>
+            </div>
+         <p><AiOutlineMenu size="24"/></p>
         </section>
     )
 }
