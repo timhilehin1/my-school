47a85c6a6793c2ef81f6e27c77be24ea8511b6b3
Timilehin <timilehinoladapo0@gmail.com>
2023-04-18 07:42:02 +0100

    progress : dropdown

---
+++ src/Components/NavItems.jsx
@@ -0,0 +1,72 @@
+export const NavItems = [ 
+    {
+        id:1,
+        title:"Home",
+        path:"./",
+        cName:"nav-items"
+    },
+
+    {
+        id:2,
+        title:" Academics",
+        path:"./Academics",
+        cName:"nav-items"
+    },
+
+    {
+        id:3,
+        title:"Admissions",
+        path:"./Admissions",
+        cName:"nav-items"
+    },
+
+    {
+        id:4,
+        title:"Academic Calendar",
+        path:"./Calendar",
+        cName:"nav-items"
+    },
+
+    {
+        id:5,
+        title:"About",
+        path:"./About",
+        cName:"nav-items"
+    },
+
+    {
+        id:6,
+        title:"Contact",
+        path:"./Contact",
+        cName:"nav-items"
+    }
+]
+
+
+export const DropdownItems = [
+    {
+        id:1,
+        title:"Faculty",
+        path:"./Faculty",
+    },
+    {
+        id:2,
+        title:"Major & Program",
+        path:"./Program",
+    },
+    {
+        id:3,
+        title:"Alumni",
+        path:"./Alumni",
+    },
+    {
+        id:4,
+        title:"Teachers",
+        path:"./Teachers",
+    },
+    {
+        id:5,
+        title:"News",
+        path:"./News",
+    },
+]
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
 import logo from '../assets/small logo.png'
+import { NavItems } from './NavItems';
 import { AiOutlineMenu} from "react-icons/ai";
-
+import { IoIosArrowDown } from "react-icons/io";
 
 
 function Navbar(){
-
     return (
-        <section className="flex px-2 py-3 header justify-between  items-center container mx-auto">
-            <div className='flex gap-1.5 items-center'>
+        <>
+        <section className="flex md:hidden px-2 py-3 header justify-between  items-center container mx-auto">
+            <div className='flex  gap-1.5 items-center'>
                 <img className='h-11' src={logo} alt="School's logo" />
                 <p>Victoria</p>
             </div>
          <p><AiOutlineMenu size="24"/></p>
         </section>
+
+        <section className=' hidden lg:flex  py-3 header justify-around items-center container mx-auto'>
+        <div className='flex  gap-1.5 items-center'>
+                <img className='h-11' src={logo} alt="School's logo" />
+                <p>Victoria</p>
+        </div>
+
+        <ul className="flex gap-6 nav-content items-center">
+            {NavItems.map(el=>{
+               
+                return <li key={el.id} className='flex items-center'>{el.title}<IoIosArrowDown style={{fontSize: "1.3em"}}/></li>
+            })}
+                {/* <li className='flex items-center'>Home<IoIosArrowDown style={{fontSize: "1.3em"}}/></li>
+                <li className='flex items-center'>Academics<IoIosArrowDown style={{fontSize: "1.3em"}}/></li>
+                <li className='flex items-center'>Admissions<IoIosArrowDown style={{fontSize: "1.3em"}}/></li>
+                <li className='flex items-center'>About<IoIosArrowDown style={{fontSize: "1.3em"}}/></li>
+                <li className='flex items-center'>Contact<IoIosArrowDown style={{fontSize: "1.3em"}}/></li> */}
+            </ul>
+        
+        <div>
+        Search
+        </div>
+
+        </section>
+        </>
     )
 }
 
